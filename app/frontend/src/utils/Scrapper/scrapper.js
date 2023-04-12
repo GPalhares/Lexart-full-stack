401763cf0244dd85513967a7be8bfaaf1e6a1e93
Gabriel <devpalhares@gmail.com>
2023-04-12 19:42:50 -0300

    refator : Web Scrap refator

--- app/frontend/src/utils/Scrapper/scrapper.js
+++ app/frontend/src/utils/Scrapper/scrapper.js
@@ -1,45 +1,33 @@
 const puppeteer = require('puppeteer');
 
 async function webScrap(category, search = '') {
-  const browser = await puppeteer.launch();
+  const itens = {};
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  // Buscapé doesn't have a separate category for search, so I used only search.
   await page.goto(`https://www.buscape.com.br/search?q=${category + search}`);
+  itens.price = await page.$$eval('.Text_MobileHeadingS__Zxam2', (element) =>
+    element.map((price) => price.innerHTML)
+  );
+  itens.title = await page.$$eval(
+    '.SearchCard_ProductCard_Name__ZaO5o',
+    (element) => element.map((title) => title.innerHTML)
+  );
+  itens.link = await page.$$eval(
+    '.SearchCard_ProductCard_Inner__7JhKb',
+    (element) => element.map((link) => link.href)
+  );
 
-  const items = await page.evaluate(() => {
-    const items = [];
-    const elements = document.querySelectorAll('.Hits_Wrapper__3q_7P > div');
+  itens.image = await page.$$eval(
+    '.SearchCard_ProductCard_Image__ffKkn span img',
+    (element) => element.map((link) => link.src)
+  );
 
-    for (let i = 0; i < elements.length; i++) {
-      const el = elements[i];
-      const item = {};
-      const imageElement = el.querySelector('span');
-      const linkElement = el.querySelector(
-        '.SearchCard_ProductCard_Inner__7JhKb'
-      );
-
-      if (imageElement) {
-        item.title = el.querySelector('img').getAttribute('alt');
-        let thumbNail = el.querySelector('img').getAttribute('src');
-        item.thumbnail = thumbNail;
-        item.price = el
-          .querySelector('[data-testid="product-card::price"]')
-          .textContent.trim();
-        items.push(item);
-      }
-
-      if (linkElement) {
-        item.permalink = linkElement.getAttribute('href');
-        items.push(item);
-      }
-    }
-
-    return items;
-  });
+  console.log(itens);
 
   await browser.close();
-  console.log(items);
-  return items;
+  return itens;
 }
 
 module.exports = webScrap;
+
+webScrap('celular', '');
